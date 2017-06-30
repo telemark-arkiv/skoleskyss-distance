a3c82f001290a7f73d834da2a2f7e9570710bfd2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-30 14:59:01 +0200

    Adds missing env

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  DISTANCE_API_KEY: process.DISTANCE_API_KEY || 'distance-api-key',
+  DISTANCE_API_KEY: process.env.DISTANCE_API_KEY || 'distance-api-key',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'sms-service',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
